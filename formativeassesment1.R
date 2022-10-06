d7f67d59afa0d1b79f08b3b45370b62e3261c575
regane2 <92222931+regane2@users.noreply.github.com>
2022-10-06 13:01:57 +0100

    inital

---
+++ formativeassesment1.R
@@ -0,0 +1,5 @@
+#1. Produce a tidy data set called gp_hiv using the tools in tidyverse that we introduced in the week 3 practical. 
+
+`indicator.hiv.estimated.prevalence%.15.49` <- read.csv("~/Downloads/indicator hiv estimated prevalence% 15-49.csv")
+View(`indicator.hiv.estimated.prevalence%.15.49`)
+gp_hiv <- `indicator.hiv.estimated.prevalence%.15.49`
